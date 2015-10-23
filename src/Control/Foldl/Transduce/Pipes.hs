8fa7131c1a940255453087c211453b20aa5463f3
ddc <diaz_carrete@yahoo.com>
2015-10-23 19:20:28 +0200

    premapEnumerableP

--- src/Control/Foldl/Transduce/Pipes.hs
+++ src/Control/Foldl/Transduce/Pipes.hs
@@ -1,5 +1,9 @@
 ﻿module Control.Foldl.Transduce.Pipes (
         FoldP
+    ,   foldP
+    ,   premapP
+    ,   premapFoldableP
+    ,   premapEnumerableP
     ) where
 
 import Control.Foldl.Transduce.Pipes.Internal 
--- src/Control/Foldl/Transduce/Pipes/Internal.hs
+++ src/Control/Foldl/Transduce/Pipes/Internal.hs
@@ -8,6 +8,10 @@
 
 module Control.Foldl.Transduce.Pipes.Internal (
         FoldP
+    ,   foldP
+    ,   premapP
+    ,   premapFoldableP
+    ,   premapEnumerableP
     ) where
 
 import Data.Bifunctor
@@ -34,14 +38,15 @@ data FoldP_ b e a =
 
 instance Applicative (FoldP b e) where
     pure a = FoldP (pure a)
-    (FoldP fa) <*> (FoldP a) = FoldP (fa <*> a)
+    FoldP fa <*> FoldP a = FoldP (fa <*> a)
 
 instance Applicative (FoldP_ b e) where
     pure a = ExhaustiveCont (\producer -> do
         r <- runEffect (producer >-> Pipes.drain)
         pure (Right (a,r)))
 
-    s1 <*> s2 = bifurcate (nonexhaustive s1) (nonexhaustive s2)  
+    TrueFold f1 <*> TrueFold f2 = TrueFold (f1 <*> f2)
+    s1 <*> s2 = bifurcate (nonexhaustiveCont s1) (nonexhaustiveCont s2)  
         where 
         bifurcate fs as = ExhaustiveCont (\producer -> do
             (outbox1,inbox1,seal1) <- spawn' (bounded 1)
@@ -54,13 +59,69 @@ instance Applicative (FoldP_ b e) where
                 Conceit (as (fromInput inbox2) `finally` atomically seal2)
                 <*>
                 (_Conceit $
-                    (runEffect (producer >-> Pipes.tee (toOutput outbox1 >> Pipes.drain) 
-                                         >->           (toOutput outbox2 >> Pipes.drain)))
+                    (runEffect (producer >-> Pipes.tee (toOutput outbox1 *> Pipes.drain) 
+                                         >->           (toOutput outbox2 *> Pipes.drain)))
                     `finally` atomically seal1 
                     `finally` atomically seal2))
 
+instance Bifunctor (FoldP_ b) where
+  bimap f g s = case s of
+      TrueFold (Foldl.FoldM step start done) -> TrueFold (Foldl.FoldM 
+          (\previous input -> withExceptT f (step previous input))
+          (withExceptT f start)
+          (\final -> withExceptT f (fmap g (done final))))
+      ExhaustiveCont u -> ExhaustiveCont (fmap (liftM  (bimap f (bimap g id))) u)
+      NonexhaustiveCont h -> NonexhaustiveCont (fmap (liftM  (bimap f g)) h)
 
-nonexhaustive :: FoldP_ b e a -> Producer b IO () -> IO (Either e a)
-nonexhaustive (TrueFold e) = \producer -> runExceptT (Foldl.impurely Pipes.foldM e (hoist lift producer))
-nonexhaustive (ExhaustiveCont e) = \producer -> liftM (fmap fst) (e producer)
-nonexhaustive (NonexhaustiveCont u) = u
+instance Bifunctor (FoldP b) where
+  bimap f g (FoldP s) = FoldP (case s of
+      Pure a -> Pure (g a)
+      Other o -> Other (bimap f g o))
+
+instance (Monoid a) => Monoid (FoldP b e a) where
+   mempty = pure mempty
+   mappend s1 s2 = (<>) <$> s1 <*> s2
+
+nonexhaustiveCont :: FoldP_ b e a -> Producer b IO () -> IO (Either e a)
+nonexhaustiveCont (TrueFold e) = \producer -> runExceptT (Foldl.impurely Pipes.foldM e (hoist lift producer))
+nonexhaustiveCont (ExhaustiveCont e) = \producer -> liftM (fmap fst) (e producer)
+nonexhaustiveCont (NonexhaustiveCont u) = u
+
+exhaustiveCont :: FoldP_ b e a -> Producer b IO r -> IO (Either e (a,r))
+exhaustiveCont s = case s of 
+    TrueFold e -> \producer -> 
+        runExceptT (Foldl.impurely Pipes.foldM' e (hoist lift producer))
+    ExhaustiveCont e -> e
+    NonexhaustiveCont activity -> \producer -> do 
+        (outbox,inbox,seal) <- spawn' (bounded 1)
+        runConceit $ 
+            (,) 
+            <$>
+            Conceit (activity (fromInput inbox) `finally` atomically seal)
+            <*>
+            (_Conceit $
+                (runEffect (producer >-> (toOutput outbox *> Pipes.drain)) 
+                `finally` atomically seal))
+
+foldP :: FoldP b e a -> Producer b IO r -> IO (Either e (a,r))
+foldP (FoldP (unLift -> s)) = exhaustiveCont s
+
+premapP :: (a -> b) -> FoldP b e r -> FoldP a e r 
+premapP f (FoldP s) = FoldP $ case s of
+    Pure p -> Pure p
+    Other o -> Other $ case o of
+        TrueFold e -> TrueFold $ Foldl.premapM f e
+        ExhaustiveCont e -> ExhaustiveCont $ \producer ->
+            e $ producer >-> Pipes.map f
+        NonexhaustiveCont ne -> NonexhaustiveCont $ \producer ->
+            ne $ producer >-> Pipes.map f
+
+premapFoldableP :: Foldable f => (a -> f b) -> FoldP b e r -> FoldP a e r 
+-- could be more efficient for TrueFold
+premapFoldableP unwinder = premapEnumerableP (Select . each . unwinder)
+
+premapEnumerableP :: Enumerable t => (a -> t IO b) -> FoldP b e r -> FoldP a e r 
+premapEnumerableP unwinder s = 
+    FoldP (Other (ExhaustiveCont (foldP s . flip for (enumerate . toListT . unwinder))))
+
+
