b618d8f4abaa66701b5e34f96911e96961bf9fbc
ddc <diaz_carrete@yahoo.com>
2015-10-23 09:27:32 +0200

    nonexhaustive

--- src/Control/Foldl/Transduce/Pipes/Internal.hs
+++ src/Control/Foldl/Transduce/Pipes/Internal.hs
@@ -15,6 +15,7 @@ import Data.Monoid
 import Control.Applicative
 import Control.Applicative.Lift
 import Control.Monad
+import Control.Monad.Trans.Except
 import qualified Control.Foldl as Foldl
 import Control.Concurrent
 import Control.Concurrent.Conceit
@@ -26,8 +27,9 @@ import Pipes.Concurrent
 newtype FoldP b e a = FoldP (Lift (FoldP_ b e) a) deriving (Functor)
 
 data FoldP_ b e a = 
-         Exhaustive (forall r. Producer b IO r -> IO (Either e (a,r)))
-       | Nonexhaustive (Producer b IO () -> IO (Either e a))
+         TrueFold (Foldl.FoldM (ExceptT e IO) b a)
+       | ExhaustiveCont (forall r. Producer b IO r -> IO (Either e (a,r)))
+       | NonexhaustiveCont (Producer b IO () -> IO (Either e a))
        deriving (Functor)
 
 instance Applicative (FoldP b e) where
@@ -35,13 +37,13 @@ instance Applicative (FoldP b e) where
     (FoldP fa) <*> (FoldP a) = FoldP (fa <*> a)
 
 instance Applicative (FoldP_ b e) where
-    pure a = Exhaustive (\producer -> do
+    pure a = ExhaustiveCont (\producer -> do
         r <- runEffect (producer >-> Pipes.drain)
         pure (Right (a,r)))
 
     s1 <*> s2 = bifurcate (nonexhaustive s1) (nonexhaustive s2)  
         where 
-        bifurcate fs as = Exhaustive (\producer -> do
+        bifurcate fs as = ExhaustiveCont (\producer -> do
             (outbox1,inbox1,seal1) <- spawn' (bounded 1)
             (outbox2,inbox2,seal2) <- spawn' (bounded 1)
             runConceit $
@@ -59,5 +61,6 @@ instance Applicative (FoldP_ b e) where
 
 
 nonexhaustive :: FoldP_ b e a -> Producer b IO () -> IO (Either e a)
-nonexhaustive (Exhaustive e) = \producer -> liftM (fmap fst) (e producer)
-nonexhaustive (Nonexhaustive u) = u
+nonexhaustive (TrueFold e) = \producer -> runExceptT (Foldl.impurely Pipes.foldM e (hoist lift producer))
+nonexhaustive (ExhaustiveCont e) = \producer -> liftM (fmap fst) (e producer)
+nonexhaustive (NonexhaustiveCont u) = u
