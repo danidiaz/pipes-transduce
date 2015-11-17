01e132bfd8993c4d43f2a7dac87382c000fbedf9
ddc <diaz_carrete@yahoo.com>
2015-11-17 09:23:11 +0100

    Added lines.

--- src/Pipes/Transduce/Text.hs
+++ src/Pipes/Transduce/Text.hs
@@ -1,1 +1,32 @@
-﻿module Pipes.Transduce.Text () where
+﻿module Pipes.Transduce.Text (lines) where
+
+import Prelude hiding (lines)
+import Data.Bifunctor
+import Data.Monoid
+import Data.Void
+import Data.Foldable
+import Data.Text hiding (lines)
+import Control.Applicative
+import Control.Applicative.Lift
+import Control.Monad
+import Control.Monad.Trans.Except
+import Control.Monad.Trans.Free hiding (Pure)
+import qualified Control.Foldl as Foldl
+import Control.Exception
+import Pipes 
+import qualified Pipes.Text
+import Pipes.Lift (distribute) 
+import Pipes.Prelude
+import qualified Pipes.Prelude as Pipes
+import qualified Pipes.Group as Pipes
+import qualified Pipes.Parse
+import Pipes.Concurrent
+import Lens.Family (view)
+
+import Pipes.Transduce
+
+lines 
+    :: TransducerP Continuous a e Text -- ^
+    -> TransducerP Delimited a e Text -- ^
+lines sometrans = delimit (view Pipes.Text.lines) sometrans
+
